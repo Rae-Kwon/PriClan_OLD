5f08c19a6105f65dd4e2388ad4d33b6dd47ded4c
Richard Kwon <rkminjae@gmail.com>
2022-04-01 22:43:52 -0400

    create User model and schema

---
+++ app/server/models/user.server.ts
@@ -0,0 +1,21 @@
+import { mongoose } from "../services/db.server";
+
+type User = {
+	_id?: mongoose.Types.ObjectId;
+	username: string;
+	passwordHash: string;
+	createdAt: Date;
+	updatedAt: Date;
+};
+
+const userSchema = new mongoose.Schema({
+	username: { type: String, required: true },
+	passwordHash: { type: String, required: true },
+	createdAt: { type: Date, required: false },
+	updatedAt: { type: Date, required: false },
+});
+
+const UserModel = mongoose.model<User>("User", userSchema);
+
+export { UserModel };
+export type { User };
